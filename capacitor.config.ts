3e8a7edcc6ad5584e009acfa47bc4942d83c2688
Admin <MoloodNaseralmemarin@gmail.com>
2023-03-10 08:46:15 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Admin.DrShop2City-FrontEnd',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
